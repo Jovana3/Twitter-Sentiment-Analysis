5fda2d145b53c2bcbbd6f6d15e67d3b2e2a63b93
Apple <apple@Apples-MacBook-Air.local>
2017-05-27 20:34:34 +0200

    Ispravka - Upoređeni algoritmi korišćenjem kros validacije na novim podacima.

--- twitter_sentiment.R
+++ twitter_sentiment.R
@@ -2,15 +2,12 @@
 library(twitteR)
 library(readr)
 
-library(dplyr)
-library(e1071)
-library(caret)
-library(pander)
-
 library(tm)
 library(wordcloud)
-library(magrittr)
+
+library(caret)
 library(RTextTools)
+
 
 
 # AUTHORIZATION
@@ -24,27 +21,28 @@ setup_twitter_oauth(twitter_auth$api_key, twitter_auth$api_secret, twitter_auth$
 trends <-availableTrendLocations()
 
 #Getting trend in the wordl (woeid -> 1)
-worldTrend <-getTrends(1)
+worldTrend <-getTrends(44418)
 
 #view trend limit
 rateLimit <-getCurRateLimitInfo()
 
 #Getting  positive tweets
-tweets <- searchTwitter('#RussianGP :)', n=10000, lang='en')
+#+#NowPlaying+exclude:retweets'
+tweetsPosS <- searchTwitter(':)+storm+exclude:retweets', n=3000, lang='en')
 class(tweets)
-positive_tweets <-twListToDF(tweets)
+positive_tweets <-twListToDF(tweetsPosS)
 positive_tweets <- subset(positive_tweets, select=c("created", "id", "text", "screenName", "retweetCount"))
 
 #write tweets to cvs file
-write_excel_csv(positive_tweets, 'data/positiveTweets.csv')
+write_excel_csv(positive_tweets, 'data/positiveTweetsStorm.csv')
 
 #Getting  negative tweets
-tweets2 <- searchTwitter('#RussianGP :(', n=10000, lang='en')
-negative_tweets <-twListToDF(tweets2)
+tweetsNegS <- searchTwitter(':(+storm+exclude:retweets', n=3000, lang='en')
+negative_tweets <-twListToDF(tweetsNegS)
 negative_tweets <- subset(negative_tweets, select=c("created", "id", "text", "screenName", "retweetCount"))
 
 #write tweets to cvs file
-write_excel_csv(negative_tweets, 'data/negativeTweets.csv')
+write_excel_csv(negative_tweets, 'data/negativeTweetsStorm.csv')
 
 
 # --CLEANING TWEETS--
@@ -56,22 +54,30 @@ negative_tweets <- negative_tweets["text"]
 all_tweets <-c(positive_tweets$text, negative_tweets$text)
 
 cleanTweets <- function(tweets){
-  #remove retweet entities
-  clean_tweets <- gsub("(RT|via)((?:\\b\\W*@\\w+)+:)", "", tweets) 
-  #remove @People
-  clean_tweets = gsub("@\\w+", "", clean_tweets) 
-  #remove punctations symbols
+  #remove emoticons
+  clean_tweets <- gsub("[:;=B](')*(-)*[)(P3DSO*sp]+", "", tweets)
+  #remove hashtags 
+  clean_tweets <- gsub("#\\w+", "", clean_tweets)
+  # remove links
+  clean_tweets <- gsub("https?:\\/\\/.*[\\r\\n]*", "", clean_tweets)
+  # remove @People
+  clean_tweets = gsub("@\\w+", "", clean_tweets)
+  # remove punctations symbols
   clean_tweets <- gsub("[[:punct:]]", "", clean_tweets)
   # remove numbers
   clean_tweets <- gsub("[[:digit:]]", "", clean_tweets)
-  #remove links
-  clean_tweets <- gsub("http\\w+", "", clean_tweets) 
-  #remove line breaks
+  # remove line breaks
   clean_tweets <- gsub("\r?\n|\r", " ", clean_tweets)
-  
+
   clean_tweets <- iconv(clean_tweets, "UTF-8", "ASCII", sub = "")
   #remove query word
-  clean_tweets <- gsub("russiangp", "", tolower(clean_tweets))
+  clean_tweets <- gsub("storm", "", tolower(clean_tweets))
+  # remove leading whitespaces
+  clean_tweets <- gsub("^[[:space:]]*","", clean_tweets) 
+  # remove trailing whitespaces
+  clean_tweets <- gsub("[[:space:]]*$","", clean_tweets)
+  # remove extra whitespaces
+  clean_tweets <- gsub(' +', ' ',clean_tweets) 
   #remove duplicates
   clean_tweets <- unique(clean_tweets)
   return(clean_tweets)
@@ -81,14 +87,15 @@ pos_tweets_clean <- cleanTweets(positive_tweets$text)
 neg_tweets_clean <- cleanTweets(negative_tweets$text)
 all_tweets_clean <- c(pos_tweets_clean, neg_tweets_clean)
 
-#create dataframe from clean tweets with class column 
+
+# create dataframe from clean tweets with class column 
 nrows_pos <- length(pos_tweets_clean)
 nrows_neg <- length(neg_tweets_clean)
 label_vec <- c(rep("pos",  nrows_pos), rep.int("neg", nrows_neg))
 clean_tweets_df <- data.frame(text = all_tweets_clean, class = label_vec, stringsAsFactors = FALSE)
 
 #write processed tweets to csv file
-write_excel_csv(clean_tweets_df, 'data/cleanTweets.csv')
+write_excel_csv(clean_tweets_df, 'data/cleanTweetsStorm.csv')
 
 
 findFreqWords <- function(tweets){
@@ -106,7 +113,7 @@ word_frequence <- findFreqWords(all_tweets_clean)
 
 createWordCloud <-function(text_corpus){
   
-  wordcloud(text_corpus, scale=c(4,0.5), max.words=100, random.order=FALSE, 
+  wordcloud(text_corpus, scale=c(3,0.2), max.words=100, random.order=FALSE, 
             rot.per=0.35, use.r.layout=FALSE, colors=brewer.pal(8, "Dark2"))
   
 }
@@ -115,13 +122,11 @@ createAndCleanCorpus <-function(tweets){
   
   #create a corpus from character vectors
   text_corpus = VCorpus(VectorSource(tweets))
-  #creeate vector of irrelevant words 
-  irr_words <- c("f", "russia", "russiangrandprix", "russiangp", "russian", "grand","prix")
   
   # -TRANSFORMATIONS-
-  #remove words stopwords and irrelevant words
+  #remove words stopwords
   clean_corpus <- text_corpus %>% 
-    tm_map(removeWords, c(stopwords("english"), irr_words)) %>% 
+    tm_map(removeWords, c(stopwords("english"))) %>% 
     #eliminate extra whitespaces
     tm_map(stripWhitespace)
   return (clean_corpus)
@@ -145,38 +150,15 @@ clean_tweets_df$class <- as.factor(clean_tweets_df$class)
 # create and clean up the corpus
 clean_corpus <-createAndCleanCorpus(clean_tweets_df$text)
 
+# create Document-Term matrix
+dtm <- DocumentTermMatrix(clean_corpus)
+
 # --FEATURE SELECTION--
-freq <- findFreqTerms(dtm_train, 50)
+freq <- findFreqTerms(dtm, 5)
 length((freq))
 
 # create Document-Term matrix
 dtm<- DocumentTermMatrix(clean_corpus, control=list(dictionary = freq))
-
-#partitioning the data for training and testing purposes
-train_index <- createDataPartition(clean_tweets_df$class, p=0.80, list=FALSE)
-
-df_train <- clean_tweets_df[train_index,]
-df_test <- clean_tweets_df[-train_index,]
-
-dtm_train <- dtm[train_index,]
-dtm_test <- dtm[-train_index,]
-
-clean_corpus_train <- clean_corpus[train_index]
-clean_corpus_test <- clean_corpus[-train_index]
-
-# a utility function for % freq tables
-frqtab <- function(x, caption) {
-  round(100*prop.table(table(x)), 1)
-}
-
-# making sure that procedure keeps the proportions 
-orig <- frqtab(clean_tweets_df$class)
-train <- frqtab(df_train$class)
-test <- frqtab(df_test$class)
-df <- as.data.frame(cbind(orig, train, test))
-colnames(df) <- c("Original", "Training set", "Test set")
-pander(df, style="rmarkdown",caption=paste0("Comparison of sentiment class frequencies among datasets"))
-
 
 # function to convert the word frequencies to yes (presence) and no (absence) labels
 convert_count <- function(x) {
@@ -185,126 +167,71 @@ convert_count <- function(x) {
   y
 }
 
-# apply the convert_count function to get final training and testing DTMs
-trainNB <- apply(dtm_train_nb, 2, convert_count)
-testNB <- apply(dtm_test_nb, 2, convert_count)
+# apply the convert_count function to get final DTM
+dtm <- apply(dtm, 2, convert_count)
+
 
 # NAIVE BAYES
 
 
-# train the  Naive Bayes classifier
-system.time(classifierNB <- naiveBayes(trainNB, df_train$class, laplace = 1))
-
-# use the NB classifier we built to make predictions on the test set.
-system.time(prediction <- predict(classifierNB, newdata=testNB))
-
-# create a table by tabulating the predicted class labels with the actual class labels
-table("Predictions"= prediction,  "Actual" = df_test$class )
-
-# prepare the confusion matrix
-conf_mat <- confusionMatrix(prediction, df_test$class)
-
-# prediction Accuracy
-conf_mat$overall['Accuracy']
-# 0.444 
-
 # Train NB classifier using caret package with 10-fold cross validation 
 control <- trainControl(method="cv", 10)
 set.seed(2)
-modelNB <- train(trainNB, df_train$class, method="nb", trControl=control)
+modelNB <- train(dtm, clean_tweets_df$class, method="nb", trControl=control)
 modelNB
 
 #Naive Bayes 
 
-#7346 samples
-#167 predictor
-#2 classes: 'neg', 'pos' 
 
-#No pre-processing
-#Resampling: Cross-Validated (10 fold) 
-#Summary of sample sizes: 6611, 6611, 6611, 6612, 6611, 6611, ... 
-#Resampling results across tuning parameters:
+# 1186 samples
+# 325 predictor
+# 2 classes: 'neg', 'pos' 
+
+# No pre-processing
+# Resampling: Cross-Validated (10 fold) 
+# Summary of sample sizes: 1068, 1067, 1068, 1068, 1067, 1067, ... 
+# Resampling results across tuning parameters:
   
-#  usekernel  Accuracy  Kappa      
-#FALSE      0.45902   -0.08180484
-#TRUE      0.45902   -0.08180484
+#  usekernel  Accuracy   Kappa    
+# FALSE      0.7723401  0.2877262
+# TRUE       0.7723401  0.2877262
 
-#Tuning parameter 'fL' was held constant at a value of 0
-#Tuning parameter
-#'adjust' was held constant at a value of 1
-#Accuracy was used to select the optimal model using  the largest value.
-#The final values used for the model were fL = 0, usekernel = FALSE and adjust = 1.
+# Tuning parameter 'fL' was held constant at a value of 0
+# Tuning parameter 'adjust' was held
+# constant at a value of 1
+# Accuracy was used to select the optimal model using  the largest value.
+# The final values used for the model were fL = 0, usekernel = FALSE and adjust = 1.
+ 
 
-# Testing the predictions
-predictNB <- predict(modelNB, testNB)
-cm <- confusionMatrix(predictNB, df_test$class, positive="pos")
-cm
-
-# Confusion Matrix and Statistics
-
-# Reference
-# Prediction neg pos
-#neg 498 605
-#pos 419 314
-
-# Accuracy : 0.4423   
-
-
-# MAX ENTROPY
-
-
-# Create the Document-term matrix using freq terms created earlier
-dtm <-DocumentTermMatrix(clean_corpus, control = list(dictionary = freq))
-dim(dtm)
 
 # create and train Max Entropy model
-container <-create_container(dtm, as.numeric(clean_tweets_df$class), trainSize = 1:6888, 
-                             testSize = 6889:9182, virgin = FALSE)  
-
-MAXENTmodel <- train_model(container, "MAXENT")
-result <- classify_model(container, MAXENTmodel)
-analytics <- create_analytics(container, result)
-summary(analytics)
-
-#ALGORITHM PERFORMANCE
-
-# MAXENTROPY_PRECISION    MAXENTROPY_RECALL    MAXENTROPY_FSCORE 
-#                0.440                0.440                0.435  
+container <-create_container(dtm, as.numeric(clean_tweets_df$class), trainSize = 1:900, 
+                             testSize = 901:1186, virgin = FALSE)  
 
 # 10-folds cross validation
 cross_validate(container, 10, "MAXENT")
 
-
 # $meanAccuracy
-# [1] 0.5082098
+# [1] 0.8941199
+
 
 
 # SUPORT VECTOR MACHINES
 
+modelSVM <- train(dtm, clean_tweets_df$class, method="svmLinear", trControl=control)
+modelSVM
 
-# Create the Document-term matrix using freq terms created earlier
-dtm <-DocumentTermMatrix(clean_corpus, control = list(dictionary = freq))
-dim(dtm)
+# Support Vector Machines with Linear Kernel 
 
-# create and train SVM model
-container <-create_container(dtm, as.numeric(clean_tweets_df$class), trainSize = 1:6888, 
-                             testSize = 6889:9182, virgin = FALSE)  
+# No pre-processing
+# Resampling: Cross-Validated (10 fold) 
+# Summary of sample sizes: 1067, 1068, 1068, 1067, 1067, 1068, ... 
+# Resampling results:
+  
+#  Accuracy   Kappa    
+# 0.7040949  0.1829585
 
-SVMmodel <- train_model(container, "SVM")
-result <- classify_model(container, SVMmodel)
-analytics <- create_analytics(container, result)
-summary(analytics)
-
-#ALGORITHM PERFORMANCE
-
-#SVM_PRECISION    SVM_RECALL    SVM_FSCORE 
-#         0.58          0.58          0.58
-
-# 10-folds cross validation
-cross_validate(container, 10, "SVM")
-
-#$meanAccuracy
-#[1] 0.3695708
+# Tuning parameter 'C' was held constant at a value of 1
 
 
 
